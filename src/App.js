459fb2048dcfd6e246f459304f680004e7a8bb05
Mustansar Iqbal <mustansar.iqbal@contour-software.com>
2022-04-10 01:59:53 +0500

    Detailspagebutton & Moviespage

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { Counter } from './features/counter/Counter';
+import React, { Fragment } from 'react';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import Detail from './components/Detail';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Home />
+      <Router>
+        <Fragment>
+        <Header />
+        <Routes>
+          <Route path='/detail' element={<Detail />} />
+          < Route path='/' element={<Home />} />
+        </Routes>
+        </Fragment>
+      </Router>
     </div>
   );
 }
