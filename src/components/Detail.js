7ba312ee85b6aa8db22dce4208066c72e9def09e
Mustansar Iqbal <mustansar.iqbal@contour-software.com>
2022-04-10 16:40:48 +0500

    DetailsPage

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -26,6 +26,12 @@ function Detail() {
                 <img src='/images/group-icon.png' />
             </GroupWatchButton>
         </Controls>
+        <SubTitle>
+            2018 - 7m - Family, Fantasy, Kids, Animation
+        </SubTitle>
+        <Description>
+            A Chineses mom who's sad when her grown son leaves home gets another chance at motherhood when one of her dumplings spings to life. But she finds that nothing stays cute and small forever.
+        </Description>
     </Container>
   )
 }
@@ -114,7 +120,20 @@ const AddButton = styled.button`
     }
 `
 
-
 const GroupWatchButton = styled(AddButton)`
     background: rgb(0, 0, 0);
+`
+
+const SubTitle = styled.div`
+    color: rgb(249, 249, 249);
+    font-size: 15px;
+    min-height: 20px;
+    margin-top: 26px;
+`
+
+const Description = styled.div`
+    line-height: 1.4;
+    font-size: 20px;
+    margin-top: 16px;
+    color: rgb(249, 249, 249);
 `
